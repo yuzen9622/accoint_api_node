ab3a64fb1d7f9145b8bf387d20ba88e809b206f6
yuzen9622 <oscar48079@gmail.com>
2025-02-15 23:24:55 +0800

    update cron jobs

--- api/accountCron.js
+++ api/accountCron.js
@@ -1,33 +1,46 @@
 const AccountModel = require("../Model/accountModel");
 const RecordModel = require("../Model/recordModel");
-
+const mongoose = require("mongoose");
 module.exports = async function (req, res) {
-  console.log("🔄 處理自動記帳...");
-  const now = new Date();
-  const autoDebitAccount = await AccountModel.find({
-    autoDebit: true,
-  });
-  if (autoDebitAccount.length === 0) return res.json("結束自動記帳");
+  console.log("連接資料庫中...");
+  try {
+    const uri = process.env.MONGOOSE_URI;
+    await mongoose.connect(uri, {
+      serverApi: { version: "1", strict: true, deprecationErrors: true },
+    });
+    console.log("連接成功!");
+    console.log("🔄 處理自動記帳...");
+    const now = new Date();
+    const autoDebitAccount = await AccountModel.find({
+      autoDebit: true,
+    });
+    if (autoDebitAccount.length === 0) return res.json("結束自動記帳");
 
-  for (let account of autoDebitAccount) {
-    if (account.autoDebitRun) {
-      if (account.autoDebitDay < now.getDate()) {
-        account.autoDebitRun = false;
-        await account.save();
+    for (let account of autoDebitAccount) {
+      if (account.autoDebitRun) {
+        if (account.autoDebitDay < now.getDate()) {
+          account.autoDebitRun = false;
+          await account.save();
+        }
+      }
+      if (account.autoDebitDay === now.getDate() && account.amount < 0) {
+        const record = new RecordModel({
+          accountId: account.toAccountId,
+          toAccountId: account._id,
+          amount: Math.abs(account.amount),
+          source: "change",
+        });
+        account.amount = 0;
+        account.autoDebitRun = true;
+        account.lastAutoDebitTime = now;
+        await Promise.all([account.save(), record.save()]);
+        console.log(
+          `✅ 自動記帳成功: ${account._id} -> ${account.toAccountId}`
+        );
       }
     }
-    if (account.autoDebitDay === now.getDate() && account.amount < 0) {
-      const record = new RecordModel({
-        accountId: account.toAccountId,
-        toAccountId: account._id,
-        amount: Math.abs(account.amount),
-        source: "change",
-      });
-      account.amount = 0;
-      account.autoDebitRun = true;
-      account.lastAutoDebitTime = now;
-      await Promise.all([account.save(), record.save()]);
-      console.log(`✅ 自動記帳成功: ${account._id} -> ${account.toAccountId}`);
-    }
+    console.log("自動記帳處理完畢!");
+  } catch (error) {
+    console.log(error);
   }
 };
